223e3788ef46f9173bee49f5f6ee88661acac9e8
Keshopan <keshopan@Keshopans-MacBook-Air.local>
2025-12-09 00:05:12 -0500

    feature: moved onSubmit api call into separate hook

--- src/screens/OnboardingFormScreen/index.tsx
+++ src/screens/OnboardingFormScreen/index.tsx
@@ -11,54 +11,16 @@ import {
   CorporationValidationResponse,
   useCorporationNumberValidation,
 } from '../../hooks/useCorporationValidation';
+import { useOnboardingSubmit } from '../../hooks/useOnboardingSubmit';
 
 const OnboardingFormScreen = () => {
   const { validateCorporationNumber } = useCorporationNumberValidation();
+  const { onSubmitHandler } = useOnboardingSubmit();
   const initialValues: FormValues = {
     firstName: '',
     lastName: '',
     phone: '',
     corporationNumber: '',
-  };
-
-  const handleSubmit = async (
-    submittedValues: FormValues,
-    { setFieldError }: FormikHelpers<FormValues>,
-  ) => {
-    try {
-      const response = await fetch(
-        'https://fe-hometask-api.qa.vault.tryvault.com/profile-details',
-        {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(submittedValues),
-        },
-      );
-
-      const contentType = response.headers.get('content-type') || '';
-      const responseData = contentType.includes('application/json')
-        ? await response.json()
-        : await response.text();
-
-      if (!response.ok) {
-        if (responseData.message.toLowerCase().includes('first name')) {
-          setFieldError('firstName', responseData.message);
-        }
-        if (responseData.message.toLowerCase().includes('last name')) {
-          setFieldError('lastName', responseData.message);
-        }
-        if (responseData.message.toLowerCase().includes('phone')) {
-          setFieldError('phone', responseData.message);
-        }
-        if (responseData.message.toLowerCase().includes('corporation')) {
-          setFieldError('corporationNumber', responseData.message);
-        }
-        return;
-      }
-      Alert.alert('Registration complete!', 'User onboarding is successful.');
-    } catch (error) {}
   };
 
   return (
@@ -70,7 +32,7 @@ const OnboardingFormScreen = () => {
           <Formik
             initialValues={initialValues}
             validationSchema={validationSchema}
-            onSubmit={handleSubmit}
+            onSubmit={onSubmitHandler}
             validateOnChange={false}
             validateOnBlur={true}
           >
